52797743f3a28ab6f95bba360d8bc497f05ab609
segundoFiuba <smolinaa@fi.uba.ar>
2023-04-22 10:11:11 -0300

    removing old project

--- ProyectoFinalPdMPdC/Drivers/PN532/src/PN532.c
+++ ProyectoFinalPdMPdC/Drivers/PN532/src/PN532.c
@@ -103,6 +103,14 @@ static uint8_t command_configureTiming [] = {0x00,0x00,0xFF, //Preamble
 		0x00
 };
 
+/**
+ * @fn bool arrays_equal(uint8_t*, uint8_t*, uint8_t)
+ * @brief checks if two arrays are equal. Usefull for validating incoming responses
+ * @param arr1 first array
+ * @param arr2 second array
+ * @param len length to compare
+ * @return true if are equal for that length, false otherwise
+ */
 static bool arrays_equal(uint8_t* arr1, uint8_t* arr2, uint8_t len) {
     for (uint8_t i = 0; i < len; i++) {
         if (arr1[i] != arr2[i]) {
@@ -122,23 +130,45 @@ static command_t pn532Driver_I2C_createCommand(uint8_t * input_command, uint8_t 
 static uint8_t calculate_lcs(uint8_t length);
 static uint8_t calculate_DCS(uint8_t *data, uint8_t len);
 
-
+/**
+ * @fn bool pn532_get_pn532Driver_initialized()
+ * @brief getter for pn532Driver_initialized
+ * @return pn532Driver_initialized
+ */
 bool pn532_get_pn532Driver_initialized(){
 	return pn532Driver_initialized;
 }
-
+/**
+ * @fn uint8_t pn532_get_pn532_address()
+ * @brief getter for pn532_address
+ * @return pn532_address
+ */
 uint8_t pn532_get_pn532_address(){
 	return pn532_address;
 }
-
+/**
+ * @fn void pn532_set_pn532_address(uint8_t)
+ * @brief setter for pn532_address
+ * @param address address to set
+ */
 void pn532_set_pn532_address(uint8_t address){
 	pn532_address = address;
 }
 
+/**
+ * @fn bool pn532_get_card_found()
+ * @brief getter for card_found
+ * @return card_found
+ */
 bool pn532_get_card_found(){
 	return card_found;
 }
 
+/**
+ * @fn bool pn532Driver_I2C_init()
+ * @brief i2c driver initializer
+ * @return true if initialized correctly
+ */
 bool pn532Driver_I2C_init(){
 	if(pn532Driver_I2C_portNucleo_init()){
 		pn532Driver_initialized=true;
@@ -147,6 +177,11 @@ bool pn532Driver_I2C_init(){
 	return false;
 }
 
+/**
+ * @fn bool pn532Driver_I2C_deinit()
+ * @brief i2c driver deinitializer
+ * @return true if deinit correctly
+ */
 bool pn532Driver_I2C_deinit(){
 	if(pn532Driver_I2C_portNucleo_deinit()){
 		pn532Driver_initialized=false;
@@ -155,18 +190,26 @@ bool pn532Driver_I2C_deinit(){
 	return false;
 }
 
+/**
+ * @fn bool receive_ACK()
+ * @brief private function to receive an ack from the pn532 and validate it
+ * @return true if ack received and validated
+ */
 static bool receive_ACK(){
 	uint8_t ackBuffer[7]={0};
 
 	if(!pn532Driver_I2C_portNucleo_receiveToBuffer(ackBuffer, LENGTH_OF_ACK_FRAME, pn532_address)){
 		return false;
 	}
-	if(!arrays_equal(pn532AckFrame,ackBuffer, LENGTH_OF_ACK_FRAME )){
-		return false;
-	}
-	return true;
+	return arrays_equal(pn532AckFrame,ackBuffer, LENGTH_OF_ACK_FRAME );
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_getFirmware(PN532_firmware_t*)
+ * @brief function to get Firmware
+ * @param firmware pointer to PN532_firmware_t where firmware info will be stored
+ * @return PN532_response_t with result of firmware read action
+ */
 PN532_response_t pn532Driver_I2C_getFirmware(PN532_firmware_t* firmware){
 	if(firmware ==  NULL) return PN532_PARAM_ERROR;
 
@@ -203,6 +246,11 @@ PN532_response_t pn532Driver_I2C_getFirmware(PN532_firmware_t* firmware){
 	return PN532_OK;
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_configureSAM()
+ * @brief function to configure SAM
+ * @return response with status of action
+ */
 PN532_response_t pn532Driver_I2C_configureSAM(){
 
 	uint8_t responseBuffer[9];
@@ -235,6 +283,12 @@ PN532_response_t pn532Driver_I2C_configureSAM(){
 	return PN532_OK;
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_listPassiveTarget(PN532_target_t*)
+ * @brief command to begin listening for devices
+ * @param target pointer to target that will be filled with target data once detected
+ * @return response with result of action
+ */
 PN532_response_t pn532Driver_I2C_listPassiveTarget(PN532_target_t * target){
 	if(target==NULL) return PN532_PARAM_ERROR;
 
@@ -290,11 +344,26 @@ PN532_response_t pn532Driver_I2C_listPassiveTarget(PN532_target_t * target){
 	searchingTarget=false;
 	return PN532_OK;
 }
-PN532_response_t pn532Driver_I2C_readMifareData_sans_target(uint8_t* buffer, uint8_t len){	card_found=false;
+
+/**
+ * @fn PN532_response_t pn532Driver_I2C_readMifareData_sans_target(uint8_t*, uint8_t)
+ * @brief read data from currently initialized target
+ * @param buffer to store the read data
+ * @param len length of the buffer
+ * @return response with result of action
+ */
+PN532_response_t pn532Driver_I2C_readMifareData_sans_target(uint8_t* buffer, uint8_t len){
 	card_found=false;
 	return pn532Driver_I2C_readMifareData(buffer, len, found_target);
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_readMifareData(uint8_t*, uint8_t, PN532_target_t)
+ * @brief read data from currently initialized target
+ * @param buffer to store the read data
+ * @param len length of the buffer
+ * @return response with result of action
+ */
 PN532_response_t pn532Driver_I2C_readMifareData(uint8_t* buffer, uint8_t len, PN532_target_t target){
 
 	uint8_t responseBuffer[100]={0x00};
@@ -343,6 +412,13 @@ PN532_response_t pn532Driver_I2C_readMifareData(uint8_t* buffer, uint8_t len, PN
 
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_readMifareData_full(uint8_t*, uint8_t)
+ * @brief function to read the full card
+ * @param buffer
+ * @param len
+ * @return response according to result
+ */
 PN532_response_t pn532Driver_I2C_readMifareData_full(uint8_t* buffer, uint8_t len){
 
 	uint8_t responseBuffer[100]={0x00};
@@ -397,6 +473,11 @@ PN532_response_t pn532Driver_I2C_readMifareData_full(uint8_t* buffer, uint8_t le
 
 }
 
+/**
+ * @fn PN532_response_t pn532Driver_I2C_configureTiming()
+ * @brief function to configure timing
+ * @return status according to result of action
+ */
 PN532_response_t pn532Driver_I2C_configureTiming(){
 
 	uint8_t responseBuffer[100]={0x00};
@@ -426,6 +507,13 @@ PN532_response_t pn532Driver_I2C_configureTiming(){
 
 }
 
+/**
+ * @fn command_t pn532Driver_I2C_createCommand(uint8_t*, uint8_t)
+ * @brief function to create the full command from the short command
+ * @param input_command short command including 0xD4
+ * @param len length of command including D4
+ * @return
+ */
 static command_t pn532Driver_I2C_createCommand(uint8_t * input_command, uint8_t len){
 	command_t output_command;
 	output_command.command[0] = 0x00;
@@ -444,10 +532,23 @@ static command_t pn532Driver_I2C_createCommand(uint8_t * input_command, uint8_t 
 	return output_command;
 }
 
+/**
+ * @fn uint8_t calculate_lcs(uint8_t)
+ * @brief function to calculate LCS checksum
+ * @param length length to calculate the checksum
+ * @return
+ */
 static uint8_t calculate_lcs(uint8_t length) {
     return ~(length-1) & 0xFF;
 }
 
+/**
+ * @fn uint8_t calculate_DCS(uint8_t*, uint8_t)
+ * @brief function to calculate DCS checksum
+ * @param data full command to calculate the checksum
+ * @param len length of the command
+ * @return
+ */
 static uint8_t calculate_DCS(uint8_t *data, uint8_t len) {
     uint8_t dcs = 0xFF;
     for (int i = 0; i < len; i++) {
